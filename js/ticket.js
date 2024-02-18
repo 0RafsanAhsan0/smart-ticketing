3a9514cd44268f7c6fb1d7b7abeba4911d355996
0RafsanAhsan0 <00rafsanahsan00@gmail.com>
2024-02-18 21:15:43 +0600

    scroll to other section

---
+++ js/ticket.js
@@ -0,0 +1,9 @@
+// scorlling to ticket section
+const buyTickets = document.getElementById("buy-ticket");
+const seatAndBill = document.getElementById("seat-bill");
+
+buyTickets.addEventListener("click",function(){
+    seatAndBill.scrollIntoView();
+})
+
+// seat select

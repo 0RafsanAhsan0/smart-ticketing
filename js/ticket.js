f1f48013cbb721619cd3a9d8a4d3504f2e488e9f
0RafsanAhsan0 <00rafsanahsan00@gmail.com>
2024-02-19 03:00:21 +0600

    total and grand total addeded

--- js/ticket.js
+++ js/ticket.js
@@ -6,7 +6,7 @@ buyTickets.addEventListener("click", function () {
   seatAndBill.scrollIntoView();
 });
 
-// -------eventListenar--------
+// -------Seat eventListenar--------
 const allSeat = document.getElementsByClassName("btn-seat");
 let count = 0;
 let count2 = 40;
@@ -24,5 +24,32 @@ for (const seat of allSeat) {
 
     // reduced total seat no.
     document.getElementById("seat-count").innerText = count2;
+
+    // showing seat and price description
+    const seatName = seat.innerText;
+    const TicketPrice = document.getElementById("t-price").innerText
+    const convertedTicketPrice = parseInt(TicketPrice);
+
+    const tDescription = document.getElementById("t-description")
+
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerText = seatName;
+    const p2 = document.createElement("p");
+    p2.innerText = convertedTicketPrice;
+
+    li.appendChild(p);
+    li.appendChild(p2);
+    tDescription.appendChild(li);
+
+    // total price
+    const totalPrice = document.getElementById("total-price").innerText;
+    const convertedTotalPrice = parseInt(totalPrice);
+    document.getElementById("total-price").innerText = convertedTotalPrice + convertedTicketPrice;
+    
+    //grand total
+    const grandTotal = document.getElementById("grand-total").innerText;
+    const convertedGrandTotal = parseInt(grandTotal);
+    document.getElementById("grand-total").innerText = convertedGrandTotal + convertedTicketPrice;
   });
 }

49c5664787a1cb171428100aff74f2f96315b42f
0RafsanAhsan0 <00rafsanahsan00@gmail.com>
2024-02-19 01:58:06 +0600

    ticket add and minus seat

--- js/ticket.js
+++ js/ticket.js
@@ -2,15 +2,27 @@
 const buyTickets = document.getElementById("buy-ticket");
 const seatAndBill = document.getElementById("seat-bill");
 
-buyTickets.addEventListener("click",function(){
-    seatAndBill.scrollIntoView();
-})
+buyTickets.addEventListener("click", function () {
+  seatAndBill.scrollIntoView();
+});
 
-// seat button color
+// -------eventListenar--------
 const allSeat = document.getElementsByClassName("btn-seat");
 let count = 0;
-for(const seat of allSeat){
-    seat.addEventListener("click", function(){
-        seat.style.backgroundColor = "#1DD100";
-    })
+let count2 = 40;
+for (const seat of allSeat) {
+  seat.addEventListener("click", function () {
+    count = count + 1;
+    count2 = count2 - 1;
+
+
+    // set button color
+    seat.style.backgroundColor = "#1DD100";
+
+    //selected ticket no.
+    document.getElementById("seat-add").innerText = count;
+
+    // reduced total seat no.
+    document.getElementById("seat-count").innerText = count2;
+  });
 }
